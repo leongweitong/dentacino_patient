fb73b367b85afbd71573f5d83f1aba3a230b0e68
leongweitong <weitong0724@gmail.com>
2024-07-01 02:05:54 +0800

    fix directory file path

--- app/SendEmail.php
+++ app/SendEmail.php
@@ -3,7 +3,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 Trait SendEmail{
     private $mail = "";
